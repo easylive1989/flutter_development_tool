9703e3079ba81011f910e65910648d3e44fef0f6
Paul <easylive1989@gmail.com>
2022-02-27 16:48:42 +0800

    reformat code

--- src/main/java/com/github/easylive1989/flutterdevelopmenttool/PopupDialogAction.java
+++ src/main/java/com/github/easylive1989/flutterdevelopmenttool/PopupDialogAction.java
@@ -7,32 +7,46 @@ import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.psi.*;
-import com.jetbrains.lang.dart.psi.DartClassDefinition;
-import com.intellij.psi.search.searches.MethodReferencesSearch;
 import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.util.Query;
+import com.jetbrains.lang.dart.psi.DartMethodDeclaration;
 import org.jetbrains.annotations.NotNull;
 
 public class PopupDialogAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent event) {
+        int offset = CommonDataKeys.CARET.getData(DataManager.getInstance().getDataContext()).getOffset();
+        PsiFile psiFile = event.getData(CommonDataKeys.PSI_FILE);
+        var message = "";
+        if (psiFile == null) {
+            show(event, message);
+            return;
+        }
+
+        PsiElement element = psiFile.findElementAt(offset);
+        if (element == null) {
+            show(event, message);
+            return;
+        }
+
+        message += String.format("\nSelected Element: %s", element);
+        message += String.format("\nSelected Language: %s", element.getLanguage());
+
+        DartMethodDeclaration method = PsiTreeUtil.getParentOfType(element, DartMethodDeclaration.class);
+        if (method == null) {
+            show(event, message);
+            return;
+        }
+
+//      Query<PsiReference> result = MethodReferencesSearch.search(containingMethod);
+        message += String.format("\nSelected Method: %s", method);
+
+        show(event, message);
+    }
+
+    private void show(AnActionEvent event, String message) {
         Project currentProject = event.getProject();
         StringBuffer dlgMsg = new StringBuffer(event.getPresentation().getText() + " Selected!");
         String dlgTitle = event.getPresentation().getDescription();
-        int offset = CommonDataKeys.CARET.getData(DataManager.getInstance().getDataContext()).getOffset();
-        PsiFile psiFile = event.getData(CommonDataKeys.PSI_FILE);
-        PsiElement element = psiFile.findElementAt(offset);
-        var message = "";
-        if (element != null) {
-            message += java.lang.String.format("\nSelected Element: %s", element);
-            message += String.format("\nSelected Language: %s", element.getLanguage());
-            DartClassDefinition containingMethod = PsiTreeUtil.getParentOfType(element, DartClassDefinition.class);
-            if (containingMethod != null) {
-//                Query<PsiReference> result = MethodReferencesSearch.search(containingMethod);
-                message += String.format("\nSelected Method: %s", containingMethod.getFirstChild());
-            }
-        }
-
         dlgMsg.append(message);
         Messages.showMessageDialog(currentProject, dlgMsg.toString(), dlgTitle, Messages.getInformationIcon());
     }
